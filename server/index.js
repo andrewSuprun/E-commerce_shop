987706448ac99907ad0fb7722eef83d99004a9a9
Andrii Suprun <suprunand2016@gmail.com>
2021-12-25 13:49:38 +0100

    did diagram and DB models

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
+const models = require('./models/models');
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'Hello Viewer'})
+})
 
 const start = async () => {
   try{
